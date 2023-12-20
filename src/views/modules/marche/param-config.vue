fc54bd1f6e3fa8315422bf52ef56126c607d76df
darius ZEKPA <dariuszekpa@email.com>
2023-12-20 16:51:28 +0100

    review

--- src/views/modules/marche/param-config.vue
+++ src/views/modules/marche/param-config.vue
@@ -11,22 +11,22 @@
 
 
         <div>
-        <div class="flex ">
-    <div class="flex-1  border">
+        <div class="flex shadow-md rounded-md ">
+    <div class="flex-1  border  ">
         <div class="border-b p-2 ">
             <h4 class="col-span-12 font-semibold text-center ">Marchés</h4>
         </div>
         <div class="grid grid-cols-2 gap-4 m-4">
-            <RouterLink to="type-marche" class="border p-2  ">
+            <RouterLink to="type-marche" class="border border-unstim-primary p-2  ">
                 <span class="">Types de Marchés</span>
             </RouterLink>
-            <RouterLink to="situation-marche" class="border p-2  ">
+            <RouterLink to="situation-marche" class="border border-unstim-primary p-2  ">
                 <span class="">Situation des Marchés</span>
             </RouterLink>
-            <RouterLink to="methode-selection" class="border p-2  ">
+            <RouterLink to="methode-selection" class="border border-unstim-primary p-2 ">
                 <span class="">Méthode de sélection</span>
             </RouterLink>
-            <RouterLink to="bailleur-marche" class="border p-2  ">
+            <RouterLink to="bailleur-marche" class="border border-unstim-primary p-2 ">
                 <span class="">Bailleurs</span>
             </RouterLink>
         </div>
@@ -37,16 +37,16 @@
             <h4 class="col-span-12 font-semibold text-center">Passation</h4>
         </div>
         <div class="grid grid-cols-2 gap-4 m-4">
-            <RouterLink to="methode-passation" class="border p-2  ">
+            <RouterLink to="methode-passation" class="border border-unstim-primary p-2 ">
                 <span class="">Méthodes de Passation</span>
             </RouterLink>
-            <RouterLink to="version-marche" class="border p-2  ">
+            <RouterLink to="version-marche" class="border border-unstim-primary p-2 ">
                 <span class="">Version de PPM</span>
             </RouterLink>
-            <RouterLink to="etape-passation" class="border p-2  ">
+            <RouterLink to="etape-passation" class="border border-unstim-primary p-2 ">
                 <span class="">Étape de Passation</span>
             </RouterLink>
-            <RouterLink to="modele-passation" class="border p-2  ">
+            <RouterLink to="modele-passation" class="border border-unstim-primary p-2 ">
                 <span class="">Modèle de Passation</span>
             </RouterLink>
         </div>
@@ -56,14 +56,14 @@
         <div class="border-b p-2">
             <h4 class="col-span-12 font-semibold text-center">Edition</h4>
         </div>
-        <div class="grid grid-cols-2 gap-4 m-4">
-            <RouterLink to="type-point-etape" class="border p-2  ">
+        <div class="grid grid-cols-2 gap-4 s m-4">
+            <RouterLink to="type-point-etape" class="border border-unstim-primary p-2 ">
                 <span class="">Types Point Etapes</span>
             </RouterLink>
-            <RouterLink to="prestataire-marche" class="border p-2  ">
+            <RouterLink to="prestataire-marche" class="border border-unstim-primary p-2 ">
                 <span class="">Prestataires</span>
             </RouterLink>
-            <RouterLink to="indicateur-performance" class="border p-2  ">
+            <RouterLink to="indicateur-performance" class="border border-unstim-primary p-2 ">
                 <span class="">Indicateurs de Performance</span>
             </RouterLink>
         </div>
